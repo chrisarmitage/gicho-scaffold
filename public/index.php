65cfd8b84c2975d80487dedc37c96d044169ab97
Chris Armitage <dev@chrisarmitage.com>
2021-06-27 22:11:43 +0100

    Use the new App initialisation

--- public/index.php
+++ public/index.php
@@ -3,23 +3,10 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Framework\App;
-use Framework\Router\RpcRouter;
-use Symfony\Component\HttpFoundation\Request;
 
 ini_set('display_errors', true);
 
-$request = Request::createFromGlobals();
+$app = new App();
 
-$container = new \Auryn\Injector();
+$app->run();
 
-$container->share($container);
-
-$container->share(new RpcRouter(null));
-
-$container->alias(\Framework\Router::class, RpcRouter::class);
-
-$app = $container->make(App::class);
-
-$response = $app->processRequest($request);
-
-$response->send();
