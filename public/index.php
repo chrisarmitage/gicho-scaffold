ed880c50632ef1817937cb14ef552bf19d37be6b
Chris Armitage <dev@chrisarmitage.com>
2022-05-31 21:43:01 +0100

    Update to Gicho 0.2

--- public/index.php
+++ public/index.php
@@ -3,11 +3,15 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Gicho\App;
+use Symfony\Component\HttpFoundation\Request;
+
 
 ini_set('display_errors', true);
 
 $rootDirectory = __DIR__ . '/..';
 $app = new App($rootDirectory);
 
-$app->run();
+$request = Request::createFromGlobals();
+$response = $app->run($request);
 
+$response->send();
